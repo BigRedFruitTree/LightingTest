0fd6efac731c370f938109cc150481f01c8e3efb
BigRedFruitTree <blaine.dooley07@gmail.com>
2025-02-10 13:40:34 -0800

    FInished Middle Area 1st Exhibit

--- Lighting Test/Assets/Scripts/UnlitTextureShader.cs
+++ Lighting Test/Assets/Scripts/UnlitTextureShader.cs
@@ -14,25 +14,97 @@ public class UnlitTextureShader : MonoBehaviour
     public Texture eight;
     public Texture nine;
     public int currentTexture = 1;
-    public Renderer renderer;
+    public new Renderer renderer;
+    public bool canChange = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+ 
     }
 
-     private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
+            if (currentTexture == 1 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = two;
+                currentTexture = 2;
+                StartCoroutine("Wait");
             
+            }
+            if (currentTexture == 2 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = three;
+                currentTexture = 3;
+                StartCoroutine("Wait");
+            }
+            if (currentTexture == 3 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = four;
+                currentTexture = 4;
+                StartCoroutine("Wait");
+            }
+            if (currentTexture == 4 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = five;
+                currentTexture = 5;
+                StartCoroutine("Wait");
+            }
+            if (currentTexture == 5 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = six;
+                currentTexture = 6;
+                StartCoroutine("Wait");
+            }
+            if (currentTexture == 6 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = seven;
+                currentTexture = 7;
+                StartCoroutine("Wait");
+            }
+            if (currentTexture == 7 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = eight;
+                currentTexture = 8;
+                StartCoroutine("Wait");
+            }
+            if (currentTexture == 8 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = nine;
+                currentTexture = 9;
+                StartCoroutine("Wait");
+            }
+            if (currentTexture == 9 && canChange == true)
+            {
+                canChange = false;
+                renderer.material.mainTexture = one;
+                currentTexture = 1;
+                StartCoroutine("Wait");
+            }
         }
     }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
+        canChange = true;
+    }
 }
+    
